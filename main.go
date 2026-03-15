dbc8df91889067af444f5c627828341a941f74a9
Kamal <Dfggxyyfdf@gmail.com>
2026-03-16 00:23:37 +0400

    Первый коммит

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	nums := make(chan int)
+	squares := make(chan int)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			num := r.Intn(101)
+			nums <- num
+		}
+		close(nums)
+
+	}()
+
+	go func() {
+		for n := range nums {
+			squares <- n * n
+		}
+		close(squares)
+	}()
+	for sg := range squares {
+		fmt.Println(sg)
+	}
+}
